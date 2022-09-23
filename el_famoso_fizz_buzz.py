4a82b76950f8a62ce8d79826c8337bb37dc89aea
Carlos Tapia <carlostapie94@gmail.com>
2022-09-23 16:31:28 -0400

    Reto 0 fizz buzz

---
+++ el_famoso_fizz_buzz.py
@@ -0,0 +1,12 @@
+for i in range(1, 100):
+    if (i % 3 == 0 and i % 5 == 0):
+        print('fizz buzz')
+
+    elif (i % 5 == 0):
+        print('buzz')
+
+    elif (i % 3 == 0):
+        print('fizz')
+
+    else:
+        print(i)
